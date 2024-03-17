1e8602d8e01cedac16aab72f3143730d642dc248
Maksim Kudaev <kudaev.m@promail366.com>
2024-03-17 19:59:18 +0300

    feat: add sky parallax

--- code/camera.py
+++ code/camera.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.math import Vector2
 from player import Player
+from pytmx.util_pygame import load_pygame
 
 
 class CameraGroup(pygame.sprite.Group):
@@ -9,6 +10,19 @@ class CameraGroup(pygame.sprite.Group):
         super().__init__()
         self.screen = pygame.display.get_surface()
         self.offset = Vector2()
+        
+        # imports
+        self.bg_sky = pygame.image.load(file='graphics/sky/bg_sky.png').convert_alpha()
+        self.fg_sky = pygame.image.load(file='graphics/sky/fg_sky.png').convert_alpha()
+        map_tmx = load_pygame(filename='data/map.tmx')
+        
+        # dimensions
+        self.padding = self.screen.get_width() / 2
+        self.sky_width = self.bg_sky.get_width()
+        self.map_width = map_tmx.width * map_tmx.tilewidth + (2 * self.padding)
+        self.sky_count = int(self.map_width // self.sky_width)
+        
+        print(self.sky_width)
     
     def draw_custom(self, player):
         if not isinstance(player, Player):
@@ -16,6 +30,11 @@ class CameraGroup(pygame.sprite.Group):
 
         self.offset.x = player.rect.centerx - (self.screen.get_width() / 2)
         self.offset.y = player.rect.centery - (self.screen.get_height() / 2)
+        
+        for sky_index in range(self.sky_count):
+            x_pos = -self.padding + (sky_index * self.sky_width)
+            self.screen.blit(source=self.bg_sky, dest=(x_pos - self.offset.x / 2.5, 800 - self.offset.y / 2.5))
+            self.screen.blit(source=self.fg_sky, dest=(x_pos - self.offset.x / 2, 800 - self.offset.y / 2))
         
         for sprite in sorted(self.sprites(), key=lambda sprite: sprite.z):
             offset_rect = sprite.image.get_rect(center=sprite.rect.center)
